787318c7432b5428bf7e3a2c2bfdd5159cb24925
Fatemeh Zahra Tavousi <66067216+FZT1999@users.noreply.github.com>
2023-07-05 11:27:58 +0330

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/FZT1999/jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
